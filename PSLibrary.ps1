9484f9947841f44a02f71e5e51d84ddbbc5e9cef
VentrisBM <batinjan@gmail.com>
2018-02-19 14:16:12 +0100

    fixed formating

--- PSLibrary.ps1
+++ PSLibrary.ps1
@@ -1,122 +1,104 @@
-﻿function Get-PrinterName
-{
+﻿function Get-PrinterName {
     Param($Computer)
-    if($Computer -eq $null)
-    {
+    if ($Computer -eq $null) {
         Get-WmiObject win32_printer | Where-Object -Property Local -EQ "True" | Select-Object -ExpandProperty Name
     }
-    else
-    {
+    else {
         Get-WmiObject win32_printer -ComputerName $Computer | Where-Object -Property Local -EQ "True" | Select-Object -ExpandProperty Name
     }
 }
 
-function Get-PrinterObject
-{
+function Get-PrinterObject {
     Param(
-    [Parameter(Mandatory=$true,Position=0)]
-    $Printer,
+        [Parameter(Mandatory = $true, Position = 0)]
+        $Printer,
     
-    [Parameter(Mandatory=$false,Position=1)]
-    $Computer)
+        [Parameter(Mandatory = $false, Position = 1)]
+        $Computer)
 
-    if($Computer -eq $null)
-    {
+    if ($Computer -eq $null) {
         Get-WmiObject -Class win32_printer -Filter "DeviceID = '$Printer'"
     }
-    else
-    {
+    else {
         Get-WmiObject -Class win32_printer -Filter "DeviceID='$Printer'" -ComputerName $Computer
     }
 }
 
-function Test-Con
-{
+function Test-Con {
     Param(
-    [Parameter(Mandatory=$true,Position=0)]
-    $Computer)
+        [Parameter(Mandatory = $true, Position = 0)]
+        $Computer)
 
     Test-Connection -ComputerName $Computer -Count 2 -Quiet
 }
 
-function Write-Message
-{
-	Param(
-    [Parameter(Mandatory=$false,Position=0)]
-    [string]
-    $Printer,
+function Write-Message {
+    Param(
+        [Parameter(Mandatory = $false, Position = 0)]
+        [string]
+        $Printer,
     
-    [Parameter(Mandatory=$false,Position=1)]
-    [string]
-    $Computer)
+        [Parameter(Mandatory = $false, Position = 1)]
+        [string]
+        $Computer)
 	
-	if(($Printer -ne "") -and ($Computer -eq ""))
-    {
+    if (($Printer -ne "") -and ($Computer -eq "")) {
         Write-Output ("$Printer printer setup completed!")
     }
-    if(($Printer -ne "") -and ($Computer -ne ""))
-    {
+    if (($Printer -ne "") -and ($Computer -ne "")) {
         Write-Output ("$Printer printer setup on computer $Computer is completed!")
     }
-    if(($Computer -ne "") -and ($Printer -eq ""))
-    {
+    if (($Computer -ne "") -and ($Printer -eq "")) {
         Write-Output ("Computer $Computer is not available.")
     }
 }
 
-function Copy-Settings
-{
+function Copy-Settings {
     Param(
-    [Parameter(Mandatory=$true,Position=0)]
-    $SetPrinter,
-    [Parameter(Mandatory=$false,Position=1)]
-    $SourcePrinter)
+        [Parameter(Mandatory = $true, Position = 0)]
+        $SetPrinter,
+        [Parameter(Mandatory = $false, Position = 1)]
+        $SourcePrinter)
 
     $SPrinterObject = Get-PrinterObject -Printer $SourcePrinter
-    if($SPrinterObject.StartTime -eq $SPrinterObject.UntilTime)
-    {
-    $SPrinterObject.StartTime = "********000000.000000+000"
-    $SPrinterObject.UntilTime = "********000000.000000+000"
+    if ($SPrinterObject.StartTime -eq $SPrinterObject.UntilTime) {
+        $SPrinterObject.StartTime = "********000000.000000+000"
+        $SPrinterObject.UntilTime = "********000000.000000+000"
     }
-    $Arguments=@{
-        StartTime=$SPrinterObject.StartTime;
-        UntilTime=$SPrinterObject.UntilTime;
-        DoCompleteFirst=$SPrinterObject.DoCompleteFirst;
-        Direct=$SPrinterObject.Direct;
-        Queued=$SPrinterObject.Queued;
-        KeepPrintedJobs=$SPrinterObject.KeepPrintedJobs;
-        RawOnly=$SPrinterObject.RawOnly;
-        EnableBIDI=$SPrinterObject.EnableBIDI;
-        Priority=$SPrinterObject.Priority;
-        SpoolEnabled=$SPrinterObject.SpoolEnabled;
-        PrintJobDataType=$SPrinterObject.PrintJobDataType;
-        Hidden=$SPrinterObject.Hidden
-        }
-Set-WmiInstance -Path "$SetPrinter" -Arguments $Arguments
+    $Arguments = @{
+        StartTime        = $SPrinterObject.StartTime;
+        UntilTime        = $SPrinterObject.UntilTime;
+        DoCompleteFirst  = $SPrinterObject.DoCompleteFirst;
+        Direct           = $SPrinterObject.Direct;
+        Queued           = $SPrinterObject.Queued;
+        KeepPrintedJobs  = $SPrinterObject.KeepPrintedJobs;
+        RawOnly          = $SPrinterObject.RawOnly;
+        EnableBIDI       = $SPrinterObject.EnableBIDI;
+        Priority         = $SPrinterObject.Priority;
+        SpoolEnabled     = $SPrinterObject.SpoolEnabled;
+        PrintJobDataType = $SPrinterObject.PrintJobDataType;
+        Hidden           = $SPrinterObject.Hidden
+    }
+    Set-WmiInstance -Path "$SetPrinter" -Arguments $Arguments
 }
 
-function Get-OSArchitecture
-{
+function Get-OSArchitecture {
     Get-WmiObject -Class Win32_OperatingSystem |Select-Object -ExpandProperty OSArchitecture
 }
 
-function Get-OSbuild
-{
+function Get-OSbuild {
     Get-WmiObject -Class Win32_OperatingSystem |Select-Object -ExpandProperty BuildNumber
 }
 
-function Get-PrintDriverName
-{
+function Get-PrintDriverName {
     Param(
-    [Parameter(Mandatory=$false,Position=0)]
-    $ComputerName)
+        [Parameter(Mandatory = $false, Position = 0)]
+        $ComputerName)
 
-    if($ComputerName -eq $null)
-    {
+    if ($ComputerName -eq $null) {
         (Get-PrinterDriver).name
     }
-    else
-    {
+    else {
         (Get-PrinterDriver -ComputerName $ComputerName).name
     }
 }
